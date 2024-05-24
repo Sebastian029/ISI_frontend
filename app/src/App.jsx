c749a45f55b03d4c8672502b5ca12d9c6ebe343f
Dominik Jaroszek <dominikjaroszekx@gmail.com>
2024-05-24 11:54:50 +0200

    admin logout

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import RequireAuth from "./comp/RequireAuth.jsx";
@@ -13,36 +12,37 @@ import ErrorPage from "./errorPage.jsx";
 import NewFlight from "./Components/AdminScreen/NewFlight/NewFlight.jsx";
 import PaymentAdmin from "./Components/AdminScreen/PaymentAdmin/PaymentAdmin.jsx";
 import FlightReservation from "./Components/FlightReservation/FlightReservation.jsx";
-import Privileges from "./Components/AdminScreen/Privileges/Privileges.jsx"
+import Privileges from "./Components/AdminScreen/Privileges/Privileges.jsx";
 import OrderConfirmation from "./Components/OrderScreen/OrderConfirmation/OrderConfirmation.jsx";
 import OrderCancel from "./Components/OrderScreen/OrderCancel/OrderCancel.jsx";
 import OrderSuccess from "./Components/OrderScreen/OrderSucess/OrderSuccess.jsx";
 import TransferDetails from "./Components/OrderScreen/OrderConfirmation/TransferDetails/TransferDetails.jsx";
-
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<RequireAuth allowedRoles={["admin"]} />}>
-          <Route path="admin" element={<AdminScreen />} />
+          <Route path="/admin" element={<AdminScreen />} />
         </Route>
         <Route element={<RequireAuth allowedRoles={["user"]} />}>
           <Route path="account" element={<AccountScreen />} />
-          <Route path="/checkout" element={<OrderConfirmation />}/>
+          <Route path="/checkout" element={<OrderConfirmation />} />
         </Route>
-          <Route path="/transferdetails" element={<TransferDetails />}/>
-          <Route path="/cancell" element={<OrderCancel />} />
-          <Route path="/success" element={<OrderSuccess />} />
-        
+        <Route path="/transferdetails" element={<TransferDetails />} />
+        <Route path="/cancell" element={<OrderCancel />} />
+        <Route path="/success" element={<OrderSuccess />} />
 
         <Route path="reservations" element={<ReservationsScreen />} />
-        <Route path="flightReservation/:flightId" element={<FlightReservation />} />
+        <Route
+          path="flightReservation/:flightId"
+          element={<FlightReservation />}
+        />
         <Route path="/unauthorized" element={<UnauthorizedScreen />} />
         <Route element={<RequireAuth allowedRoles={["admin"]} />}>
-        <Route path="/newflight" element={<NewFlight />} />
-        <Route path="/paymentadmin" element={<PaymentAdmin />} />
-        <Route path="/privileges" element={<Privileges />} />
+          <Route path="/newflight" element={<NewFlight />} />
+          <Route path="/paymentadmin" element={<PaymentAdmin />} />
+          <Route path="/privileges" element={<Privileges />} />
         </Route>
         <Route path="/" element={<HomeScreen />} />
         <Route path="*" element={<ErrorPage />}></Route>
