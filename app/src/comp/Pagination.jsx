4f625f3d4b2a1c8a281b5ccb93948bc850d046dd
Sebastian <iwan.sebastian029@gmail.com>
2024-06-20 20:44:22 +0200

    jsx error

--- app/src/comp/Pagination.jsx
+++ app/src/comp/Pagination.jsx
@@ -212,7 +212,7 @@ export default function PaginatedItems({
           />
         </div>
       </div>
-      <style jsx>{`
+      <style jsx="true">{`
         .pagination {
           display: flex;
           list-style: none;
